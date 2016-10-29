e84d0fe936c0bc01026bf8624bff3d05a547cf54
Ishan Soni <ishansoni3@gmail.com>
2016-10-29 03:43:30 -0400

    fixed a bug

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 });
 
 function auto_grow(element) {
-    element.style.height = "5px";
+    element.style.height = "40px";
     element.style.height = (element.scrollHeight)+"px";
 }
